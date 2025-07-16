cc0e442835eb5118ef6a0a0271c855513c3b3ec8
JordDim <dimelowjordan@gmail.com>
2025-07-16 21:17:27 +0200

    Upload unversioned files

---
+++ src/main/java/com/freelance/atelier_workshops/AtelierWorkshopsApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.atelier_workshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierWorkshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierWorkshopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelance/atelier_workshops/AtelierWorkshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelance.atelier_workshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierWorkshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
